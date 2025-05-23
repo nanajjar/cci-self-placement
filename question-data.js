e4d54cf6e686392ff0baad12afd5b241102e9a96
Lane Pentecost <30759236+Project-Faust@users.noreply.github.com>
2025-05-23 18:27:10 +0000

    init commit; template for question objects; module 2 question

---
+++ question-data.js
@@ -0,0 +1,26 @@
+const questionDataTemplate = [
+    {
+        title: "",
+        text: "",
+        answer: ""
+    },
+];
+
+const questionData = [
+    {
+        title: "Problem Solving & Algorithms",
+        text: "Programming is, at it's core, all about solving problems. Read the following cases and try to determine how you would approach each one.",
+        question: {
+            a: "Given a whole number (integer), determine the previous and next number.",
+            b: "Given the width and height of a right triangle, determine the area of the shape. (Area of a right triangle = 0.5 * width * height)",
+            c: "Given the cost of an order, the tip percentage, and the tax percentage, calculate the total amount to be paid.",
+            d: "",
+        },
+        answer: {
+            a: "",
+            b: "",
+            c: "",
+            d: "",
+        }
+    },
+];
